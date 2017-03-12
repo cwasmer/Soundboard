a97a80b4997301505f175254f4abe17a43acc0f4
Christian Wasmer <christian.wasmer@gmail.com>
2017-03-12 10:04:35 +0100

    Initial Commit

---
+++ Soundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soundboard
+//
+//  Created by Christian Wasmer on 12/03/2017.
+//  Copyright © 2017 Christian Wasmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
