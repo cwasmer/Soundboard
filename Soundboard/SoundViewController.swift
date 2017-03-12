76af4d72f9912cac3f440d958817f6fb47630eb3
Christian Wasmer <christian.wasmer@gmail.com>
2017-03-12 10:18:05 +0100

    Storyboard set up

---
+++ Soundboard/SoundViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SoundViewController.swift
+//  Soundboard
+//
+//  Created by Christian Wasmer on 12/03/2017.
+//  Copyright © 2017 Christian Wasmer. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
